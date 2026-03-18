ea193fc46d36fc76056e8d23e8b9511f431e48cd
Ivan Di Sante <ivan.disante@outlook.it>
2026-03-18 09:54:29 +0100

    Fixed KSP and KMP

--- composebook-plugin/src/main/java/io/github/cerrativan/composebook/plugin/ComposebookPlugin.kt
+++ composebook-plugin/src/main/java/io/github/cerrativan/composebook/plugin/ComposebookPlugin.kt
@@ -9,9 +9,22 @@ class ComposebookPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.plugins.apply("com.google.devtools.ksp")
 
-        target.dependencies.add("kspDebug", "io.github.cerrativan.composebook:composebook-processor:$VERSION")
-        target.dependencies.add("debugImplementation", "io.github.cerrativan.composebook:composebook-ui:$VERSION")
-        target.dependencies.add("implementation", "io.github.cerrativan.composebook:composebook-annotations:$VERSION")
+        var isKmp = false
+
+        target.plugins.withId("org.jetbrains.kotlin.multiplatform") {
+            isKmp = true
+            target.dependencies.add("kspAndroidDebug", "io.github.cerrativan.composebook:composebook-processor:$VERSION")
+            target.dependencies.add("androidDebugImplementation", "io.github.cerrativan.composebook:composebook-ui:$VERSION")
+            target.dependencies.add("commonMainImplementation", "io.github.cerrativan.composebook:composebook-annotations:$VERSION")
+        }
+
+        target.afterEvaluate {
+            if (!isKmp) {
+                target.dependencies.add("kspDebug", "io.github.cerrativan.composebook:composebook-processor:$VERSION")
+                target.dependencies.add("debugImplementation", "io.github.cerrativan.composebook:composebook-ui:$VERSION")
+                target.dependencies.add("implementation", "io.github.cerrativan.composebook:composebook-annotations:$VERSION")
+            }
+        }
 
         target.tasks.register("composebookRun", ComposebookRunTask::class.java) {
             group = "composebook"
